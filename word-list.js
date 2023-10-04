d5449f3acc25dc3055d24c7dc9be4bf731630b14
Yerkin23 <neo.anyway@gmail.com>
2023-10-04 15:06:51 +0600

    initial commit

---
+++ word-list.js
@@ -0,0 +1,22 @@
+const wordList = [
+  {
+    word: 'fray',
+    hint: 'to wear into shreds'
+  },
+  {
+    word: 'melodramatic',
+    hint: 'extremely or overly emotional'
+  },
+  {
+    word: 'milieu',
+    hint: 'surroundings, setting'
+  },
+  {
+    word: 'mischief',
+    hint: 'behavior or activity that annoys or bothers but is not meant to cause serious harm'
+  },
+  {
+    word: 'puny',
+    hint: 'small and weak in size or power'
+  }
+];
